3fbafa62ec98a6cc4c4e2c2babba7558ebd2574b
Anwesha Das <dasanwesha2409@gmail.com>
2025-08-02 17:38:58 +0530

    fix: initialize Prisma client in chatService

--- services/chatService.js
+++ services/chatService.js
@@ -1,5 +1,5 @@
 const { PrismaClient } = require('@prisma/client');
-
+const prisma = new PrismaClient();
 
 // Save chat to DB using ChatMessage model
 const saveChat = async (userMessage, aiMessage, type, userId) => {
